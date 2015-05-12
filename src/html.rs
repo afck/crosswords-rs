4dce1919f69ab8a8be4530d1b6fdde8b45a3f6e8
Andreas Fackler <AndreasFackler@gmx.de>
2015-05-12 12:11:27 +0300

    Cleanup HTML

--- src/html.rs
+++ src/html.rs
@@ -1,16 +1,9 @@
 use crosswords_rs::{Crosswords, Dir, Point, PrintItem};
 use std::io::{Result, Write};
 
-const HTML_START: &'static str = r#"
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html><head><style type="text/css">
-div {
-    display: inline-block;
-    position: relative;
-}
+const CSS: &'static str = r#"
 .solution {
-    font-size: 22px;
-    font-family: "monospace",monospace;
+    font: 22px monospace;
     text-align: center;
     position: absolute;
     left: 0px;
@@ -18,63 +11,58 @@ div {
     bottom: 0px;
 }
 .hint {
-    font-size: 8px;
-    font-family: "monospace",monospace;
-    text-color: light-gray;
+    font: 8px monospace;
+    color: Gray;
     position: absolute;
 }
-.gridframe {
+.row {
     overflow: hidden;
-    white-space: nowrap;
+    float: left;
 }
-.border {
-    background-color: #000088;
+.row > div:nth-child(even) { width: 30px; }
+.row > div:nth-child(odd) { width: 2px; }
+.row > div {
+    float: left;
+    position: relative;
 }
-.line {
-    background-color: #DDDDDD;
-}
-.borderwidth {
-    width: 2px;
-}
-.borderheight {
-    height: 2px;
-}
-.cellwidth {
-    width: 30px;
-}
-.cellheight {
-    height: 30px;
-}
-.blockcol {
-    background-color: #8888CC;
-}
-</style><title>CW</title></head><body>
+.low { height: 2px; }
+.high { height: 30px; }
+.dark { background-color: DarkBlue; }
+.light { background-color: LightGray; }
+.blockcol { background-color: DarkBlue; }
 "#;
-const HTML_END: &'static str = "<br></body>";
 
 fn get_border_class(border: bool) -> &'static str {
-    if border { "border" } else { "line" }
+    if border { "dark" } else { "light" }
 }
 
 fn string_for(item: PrintItem, solution: bool) -> String {
     match item {
+        PrintItem::HorizBorder(b) | PrintItem::Cross(b) =>
+            format!(r#"<div class="low {}"></div>"#, get_border_class(b)),
         PrintItem::VertBorder(b) =>
-            format!(r#"<div class="borderwidth cellheight {}"></div>"#, get_border_class(b)),
-        PrintItem::HorizBorder(b) =>
-            format!(r#"<div class="cellwidth borderheight {}"></div>"#, get_border_class(b)),
-        PrintItem::Cross(b) =>
-            format!(r#"<div class="borderwidth borderheight {}"></div>"#, get_border_class(b)),
+            format!(r#"<div class="high {}"></div>"#, get_border_class(b)),
         PrintItem::Block => 
-            format!(r#"<div class="cellwidth cellheight blockcol"></div>"#),
+            format!(r#"<div class="high blockcol"></div>"#),
         PrintItem::CharHint(c, hint) =>
-            format!(concat!(r#"<div class = "cellheight cellwidth">"#,
+            format!(concat!(r#"<div class = "high">"#,
                             r#"<span class="hint">{}</span>"#,
                             r#"<span class="solution">{}</span>"#,
                             r#"</div>"#),
                     hint.map(|h| h.to_string()).unwrap_or("".to_string()),
                     if solution { c.to_string() } else { "&nbsp;".to_string() }),
-        PrintItem::LineBreak => "<br>".to_string(),
+        PrintItem::LineBreak => r#"</div><div class="row">"#.to_string(),
     }
+}
+
+fn write_grid<T: Write, I: Iterator<Item = PrintItem>>(writer: &mut T, items: I, solution: bool)
+        -> Result<()> {
+    try!(writeln!(writer, r#"<div class="row">"#));
+    for item in items {
+        try!(writer.write_all(&string_for(item, solution).as_bytes()))
+    }
+    try!(writeln!(writer, "</div>"));
+    Ok(())
 }
 
 fn write_hints<T: Write>(writer: &mut T, cw: &Crosswords, dir: Dir) -> Result<()> {
@@ -97,21 +85,18 @@ fn write_hints<T: Write>(writer: &mut T, cw: &Crosswords, dir: Dir) -> Result<()
     Ok(())
 }
 
-fn write_grid<T: Write, I: Iterator<Item = PrintItem>>(writer: &mut T, items: I, solution: bool)
-        -> Result<()> {
-    try!(writeln!(writer, "<div class=\"gridframe\">"));
-    for item in items {
-        try!(writer.write_all(&string_for(item, solution).as_bytes()))
-    }
-    try!(writeln!(writer, "</div>"));
-    Ok(())
-}
-
 pub fn write_html<T: Write>(writer: &mut T, cw: &Crosswords, solution: bool) -> Result<()> {
-    try!(writer.write_all(HTML_START.as_bytes()));
+    try!(writeln!(writer, r#"<!doctype html>"#));
+    try!(writeln!(writer, r#"<head>"#));
+    try!(writeln!(writer, r#"<meta charset="utf-8" />"#));
+    try!(writeln!(writer, r#"<style type="text/css">{}</style>"#, CSS));
+    try!(writeln!(writer, r#"<title>Crosswords</title>"#));
+    try!(writeln!(writer, r#"</head><body>"#));
+    try!(writeln!(writer, r#"<div style="width: {}px">"#, cw.get_width() * 32 + 2));
     try!(write_grid(writer, cw.print_items(), solution));
+    try!(writeln!(writer, r#"</div><br><div style="clear: both"></div>"#));
     try!(write_hints(writer, &cw, Dir::Right));
     try!(write_hints(writer, &cw, Dir::Down));
-    try!(writer.write_all(HTML_END.as_bytes()));
+    try!(writeln!(writer, "<br></body>"));
     Ok(())
 }
