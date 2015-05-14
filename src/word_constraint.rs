b0fed69cee35d564b2660f1a82f074c8068f6c17
Andreas Fackler <AndreasFackler@gmx.de>
2015-05-14 12:26:31 +0300

    Clean up word_constraint.rs

--- src/word_constraint.rs
+++ src/word_constraint.rs
@@ -1,28 +1,43 @@
+//! A `WordConstraint` represents the subset of all words with a given length, and optionally with
+//! a given n-gram at a specific position.
+
 use cw::CVec;
 use std::iter;
 use std::slice;
 use std::ops;
 use std::option;
 
+/// The `WordConstraint` type. See [the module level documentation](index.html) for more.
 #[derive(Clone, Hash, Eq, PartialEq)]
 pub enum WordConstraint {
+    /// All words with the given length.
     Length(usize),
+    /// Words with the given character at the specified position.
     CharAt(char, usize, usize),
+    /// Words with the given bigram at the specified position.
     BigramAt([char; 2], usize, usize),
+    /// Words with the given trigram at the specified position.
     TrigramAt([char; 3], usize, usize),
+    /// Words with the given n-gram at the specified position. For n <= 3, the corresponding more
+    /// specific variant should be used to avoid ambiguity and heap-allocating the n-gram.
     NGramAt(CVec, usize, usize),
 }
 
-pub type NgramIter<'a> = iter::Map<
+type NgramIter<'a> = iter::Map<
     iter::Zip<slice::Windows<'a, char>, iter::Enumerate<iter::Repeat<usize>>>,
     fn((&[char], (usize, usize))) -> WordConstraint>;
 
-pub type AllNgramIter<'a> = iter::FlatMap<
+type AllNgramIter<'a> = iter::FlatMap<
     iter::Zip<iter::Repeat<&'a CVec>, ops::Range<usize>>,
     NgramIter<'a>,
     fn((&'a Vec<char>, usize)) -> NgramIter<'a>>;
 
+/// An iterator over all constraints applying to a given word.
+pub type AllConstraintsIter<'a> = iter::Chain<AllNgramIter<'a>, option::IntoIter<WordConstraint>>;
+
 impl WordConstraint {
+    /// Create a `WordConstraint` that specifies all words of the given length, and, if the given
+    /// n-gram is not empty, with the given n-gram at the specified position.
     pub fn with_ngram(ngram: &[char], pos: usize, len: usize) -> WordConstraint {
         match ngram.len() {
             0 => WordConstraint::Length(len),
@@ -47,8 +62,8 @@ impl WordConstraint {
         iter::repeat(word).zip(1..(max_n + 1)).flat_map(to_iter)
     }
 
-    pub fn all<'a>(word: &'a CVec, max_n: usize) ->
-            iter::Chain<AllNgramIter<'a>, option::IntoIter<WordConstraint>> {
+    /// Return an iterator over all constraints applying to a given word.
+    pub fn all<'a>(word: &'a CVec, max_n: usize) -> AllConstraintsIter<'a> {
         WordConstraint::all_ngram_constraints(word, max_n)
             .chain(Some(WordConstraint::Length(word.len())))
     }
